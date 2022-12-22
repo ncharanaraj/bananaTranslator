472c4140da6b0b312b5460b1b9d94aa959491bcd
Charanaraj N <goudacharanraj760@gmail.com>
2022-12-22 17:02:57 +0530

    Taking input and console

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-var username = prompt("Enter your name");
-alert("Our next boss is " + username);
+// var username = prompt("Enter your name");
+// alert("Our next boss is " + username);
+
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+
+function clickEvenHandler() {
+    console.log("clicked")
+    console.log("input", txtInput.value)
+}
+
+btnTranslate.addEventListener("click", clickEvenHandler);
